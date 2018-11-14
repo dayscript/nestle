4e840c375494253de17f43ffd0174106adeb8541
cdvillamil92 <cvillamil@dayscript.com>
2018-11-14 15:00:28 -0500

    M store

--- app/Store.php
+++ app/Store.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Store extends Model
 {
     protected $fillable =  [
-    	'regional', 'distribuidor', 'nit', 'nombre_cliente', 'razon_social', 'direccion', 'barrio', 'telefono', 'ciudad', 'n_visita',
+    	'regional',
+    	'distribuidor',
+    	'nit',
+    	'nombre_cliente',
+    	'razon_social',
+    	'direccion',
+    	'barrio',
+    	'telefono',
+    	'ciudad',
+    	'n_visita',
     ];
 }
